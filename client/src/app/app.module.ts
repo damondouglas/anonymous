de660cee2eb16ad538644bb850641f0ba108180e
Damon Douglas <douglas.damon@gmail.com>
2017-06-24 17:30:35 -0700

    Impl notes front end

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
+import { NotelistComponent } from './notelist/notelist.component';
+
+import { NoteService } from './note.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotelistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [NoteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
